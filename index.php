0c482027cf4167f9aac3ad481bdbe51fe1989334
Oksana-Tnt <oksanatantsiura@gmail.com>
2025-01-14 18:21:12 +0100

    fix index.php

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
-<?php
+<?php require_once __DIR__ . '/functions.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
 
-function markupCard()
-{
-    require_once "./constants.php";
-    foreach ($products as $product) : ?>
-        <div class="card">
-            <img src="<?php echo $product['img']; ?>" class="card-img-top" style="max-width: 18rem;" alt="<?php echo $product['name']; ?>">
-            <div class="card-body d-flex flex-column justify-content-between">
-                <h5 class="card-title"><?php echo $product["name"]; ?></h5>
-                <p class="card-text"><?php echo $product["description"]; ?></p>
-                <p class="card-text"><small class="text-body-secondary"><?php echo $product["price"]; ?></small></p>
-            </div>
+<body>
+
+    <div class="container mt-5">
+        <h1 class="text-center mb-3">OUR PRODUCTS</h1>
+        <div class="card-group">
+
+            <?php markupCard(); ?>
+
         </div>
+    </div>
+</body>
 
-<?php endforeach;
-}
+</html>
